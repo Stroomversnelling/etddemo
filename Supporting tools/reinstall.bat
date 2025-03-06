782c11d5e11eba76a107b081f54530db494778bd
Nicolas Dickinson <dickinson@washnote.com>
2025-03-06 10:06:47 +0100

    Added analysis and aggregation examples and supporting tools for developing examples

---
+++ Supporting tools/reinstall.bat
@@ -0,0 +1,6 @@
+pip uninstall etdanalyze
+pip uninstall etdtransform
+pip uninstall etdmap
+pip install git+https://github.com/Stroomversnelling/etdanalyze.git
+pip install git+https://github.com/Stroomversnelling/etdtransform.git
+pip install git+https://github.com/Stroomversnelling/etdmap.git
